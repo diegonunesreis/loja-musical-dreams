8e37fd1d34d5b49bcde6b9ee3062c25add2f39c3
Diego Nunes <diegonunesftw@gmail.com>
2017-11-28 13:18:41 -0200

    Commit from another computer

---
+++ LojaDeInstrumentosMusicais/Models/itemPedido.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaDeInstrumentosMusicais.Models
+{
+    public class itemPedido
+    {
+        public int Id { get; private set; }
+        public Produto Produto { get; set; }
+        public int Quantidade { get; private set; }
+        public decimal PrecoUnitario { get; private set; }
+        public decimal Subtotal {
+            get {
+                return this.Quantidade * this.PrecoUnitario;
+            }
+        }
+
+        public itemPedido(int id, Produto produto, int quantidade)
+        {
+            this.Id = id;
+            this.Produto = produto;
+            this.Quantidade = quantidade;
+            this.PrecoUnitario = produto.Preco;
+        }
+
+    }
+}
